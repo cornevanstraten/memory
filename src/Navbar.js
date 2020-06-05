2a91b1cecce02ef53cba0112df8f527eeb3ba738
Corné van Straten <vanstratenc@gmail.com>
2020-06-05 09:12:50 -0400

    turn navbar into functional component

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,24 +1,17 @@
 import React, { Component } from "react";
 import "./Navbar.css";
 
-class Navbar extends Component {
-  static defaultProps = {
-    onNewGame() {}, //function is defined in App.js
-  };
-  render() {
-    return (
-      <header>
-        <h2>
-          <a>Memory Game</a>
-        </h2>
-        <nav>
-          <li>
-            <a onClick={this.props.onNewGame}>New Game</a>
-          </li>
-        </nav>
-      </header>
-    );
-  }
-}
+const Navbar = ({ onNewGame }) => (
+  <header>
+    <h2>
+      <a>Memory Game</a>
+    </h2>
+    <nav>
+      <li>
+        <a onClick={onNewGame}>New Game</a>
+      </li>
+    </nav>
+  </header>
+);
 
 export default Navbar;
