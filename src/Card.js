a38bcffaf2410ef33d0bdfbaa809102dc5e987e5
Corné van Straten <vanstratenc@gmail.com>
2020-06-05 09:22:47 -0400

    turn card into functional component

--- src/Card.js
+++ src/Card.js
@@ -1,20 +1,14 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
+import React from "react";
 import "./Card.css";
 
-class Card extends Component {
-  render() {
-    const { color, id, hidden, cardClick } = this.props;
-    return (
-      <div
-        className={!hidden ? "card" : "card-hidden"}
-        style={{
-          backgroundColor: !hidden ? color : "gray",
-        }}
-        onClick={() => cardClick(id, color)}
-      ></div>
-    );
-  }
-}
+const Card = ({ color, id, hidden, cardClick }) => (
+  <div
+    className={!hidden ? "card" : "card-hidden"}
+    style={{
+      backgroundColor: !hidden ? color : "gray",
+    }}
+    onClick={() => cardClick(id, color)}
+  ></div>
+);
 
 export default Card;
